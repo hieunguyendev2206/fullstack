edb739a1497e682e26f9a24989412c8a681a66ec
HieuNguyenDev <2100011408@nttu.edu.vn>
2024-05-17 11:55:24 +0700

    change size image

--- frontend/src/components/Admin/LayoutAdmin.jsx
+++ frontend/src/components/Admin/LayoutAdmin.jsx
@@ -4,7 +4,6 @@ import React from "react";
 function LayoutAdmin({children}) {
     return (
         <div>
-
             {children}
         </div>
     );
--- frontend/src/pages/PersonalPage/Personal.jsx
+++ frontend/src/pages/PersonalPage/Personal.jsx
@@ -38,21 +38,25 @@ function Personal({ navigate }) {
         try {
             if (!valueUser.name || !valueUser.address || !valueUser.phone) {
                 toast.warning('Bạn phải điền đầy đủ thông tin');
-            } else {
-                const res = await updateUser(user._id, valueUser);
-                if (res?.success) {
-                    if (file) {
-                        const uploadRes = await uploadUserProfilePicture(user._id, file);
-                        if (uploadRes?.data?.success) {
-                            toast.success('Cập nhật và upload ảnh thành công');
-                            setProfilePicture(uploadRes.data.user.profilePicture);
-                            dispatch(getUser(uploadRes.data.user));
-                        }
-                    } else {
-                        toast.success('Cập nhật thành công');
-                        dispatch(getUser(res.user));
-                    }
+                return;
+            }
+
+            const updateUserPromise = updateUser(user._id, valueUser);
+            const uploadProfilePicturePromise = file ? uploadUserProfilePicture(user._id, file) : Promise.resolve(null);
+
+            const [updateRes, uploadRes] = await Promise.all([updateUserPromise, uploadProfilePicturePromise]);
+
+            if (updateRes?.success) {
+                if (uploadRes && uploadRes.data?.success) {
+                    toast.success('Cập nhật và upload ảnh thành công');
+                    setProfilePicture(uploadRes.data.user.profilePicture);
+                    dispatch(getUser(uploadRes.data.user));
+                } else {
+                    toast.success('Cập nhật thành công');
+                    dispatch(getUser(updateRes.user));
                 }
+            } else {
+                toast.error('Cập nhật thất bại');
             }
         } catch (e) {
             console.log(e);
@@ -62,6 +66,14 @@ function Personal({ navigate }) {
 
     const onDrop = (acceptedFiles) => {
         const file = acceptedFiles[0];
+        const maxFileSize = 2 * 1024 * 1024; // 2MB
+
+        if (file.size > maxFileSize) {
+            toast.warning('Kích thước tệp quá lớn. Vui lòng chọn tệp nhỏ hơn 2MB.');
+            setFile(null); // Đảm bảo giá trị file được đặt lại nếu vượt quá giới hạn
+            return;
+        }
+
         setFile(file);
         const reader = new FileReader();
         reader.onload = () => {
@@ -70,10 +82,10 @@ function Personal({ navigate }) {
         reader.readAsDataURL(file);
     };
 
-    const { getRootProps, getInputProps } = useDropzone({ onDrop, noClick: true });
+    const { getRootProps, getInputProps, open } = useDropzone({ onDrop, noClick: true });
 
     const handleIconClick = () => {
-        document.getElementById('fileInput').click();
+        open();
     };
 
     return (
@@ -81,34 +93,34 @@ function Personal({ navigate }) {
             <div className='content-personal'>
                 <div className='personal--box'>
                     <div className='personal--box--top' onClick={handleIconClick}>
-            <span style={{ backgroundImage: `url(${profilePicture})` }} className='profile-picture'>
-              {!profilePicture && <FaRegUser className='icon' />}
-            </span>
+                        <span style={{ backgroundImage: `url(${profilePicture})` }} className='profile-picture'>
+                            {!profilePicture && <FaRegUser className='icon' />}
+                        </span>
                     </div>
                     <div className='personal--box--bottom'>
-            <span>
-              <label htmlFor=''>Tên: </label>
-              <input
-                  defaultValue={user?.name}
-                  onChange={(e) => setValueUser({...valueUser, name: e.target.value})}
-              />
-            </span>
+                        <span>
+                            <label htmlFor=''>Tên: </label>
+                            <input
+                                defaultValue={user?.name}
+                                onChange={(e) => setValueUser({...valueUser, name: e.target.value})}
+                            />
+                        </span>
                         {!valueUser?.name && <p className='error-message'>Không được để trống</p>}
                         <span>
-              <label htmlFor=''>Số điện thoại: </label>
-              <input
-                  defaultValue={user?.phone}
-                  onChange={(e) => setValueUser({...valueUser, phone: e.target.value})}
-              />
-            </span>
+                            <label htmlFor=''>Số điện thoại: </label>
+                            <input
+                                defaultValue={user?.phone}
+                                onChange={(e) => setValueUser({...valueUser, phone: e.target.value})}
+                            />
+                        </span>
                         {!valueUser?.phone && <p className='error-message'>Không được để trống</p>}
                         <span>
-              <label htmlFor=''>Địa chỉ: </label>
-              <input
-                  defaultValue={user?.address}
-                  onChange={(e) => setValueUser({...valueUser, address: e.target.value})}
-              />
-            </span>
+                            <label htmlFor=''>Địa chỉ: </label>
+                            <input
+                                defaultValue={user?.address}
+                                onChange={(e) => setValueUser({...valueUser, address: e.target.value})}
+                            />
+                        </span>
                         {!valueUser?.address && <p className='error-message'>Không được để trống</p>}
                         <div {...getRootProps()} className='dropzone'>
                             <input {...getInputProps()} id='fileInput'/>
