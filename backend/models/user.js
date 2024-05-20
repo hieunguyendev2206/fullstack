07c1f8cdbf0b46e6eeb760cc80d7dd7fd7b7c9ab
HieuNguyenDev <2100011408@nttu.edu.vn>
2024-05-20 10:32:26 +0700

    change name of user

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: String, default: 'user'},
+    role: {type: String, default: 'User'},
     phone: {type: String},
     cart: [
         {
