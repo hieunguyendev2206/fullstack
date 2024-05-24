3c04a198b400df6cd37c9094b0893759b0a9cad2
HieuNguyenDev <2100011408@nttu.edu.vn>
2024-05-24 11:42:55 +0700

    change display price sale

--- frontend/src/components/card/cardProduct/CardProductCbn.jsx
+++ frontend/src/components/card/cardProduct/CardProductCbn.jsx
@@ -1,12 +1,15 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 import "./Card.scss";
-import {formatNumber} from "../../../helper/format";
+import { formatNumber } from "../../../helper/format";
 import withBase from "../../../hocs/withBase";
 
-function CardProductCbn({data, navigate}) {
+function CardProductCbn({ data, navigate }) {
     const handleNavigate = () => {
         navigate(`/product/${data._id}`);
     };
+
+    const isDiscounted = data?.discount && data.discount > 0;
+
     return (
         <div key={data?.id} className="box-card-product" onClick={handleNavigate}>
             <div className="box-card-product--image">
@@ -18,23 +21,22 @@ function CardProductCbn({data, navigate}) {
             </div>
             <div className="box-card-product--content">
                 <p className="box-card-product--content--name">{data?.name}</p>
-                {data?.discount ? (
-                    <div className="box-card-product--content--monney">
-                        <p className="box-card-product--content--monney--price">
+                <div className="box-card-product--content--monney">
+                    {isDiscounted ? (
+                        <>
+                            <p className="box-card-product--content--monney--price with-discount">
+                                {formatNumber(data?.price)}
+                            </p>
+                            <p className="box-card-product--content--monney--sale">
+                                {formatNumber(data?.price - (data?.price * data.discount) / 100)}
+                            </p>
+                        </>
+                    ) : (
+                        <p className="box-card-product--content--monney--sale no-discount">
                             {formatNumber(data?.price)}
                         </p>
-                        <p className="box-card-product--content--monney--sale">
-                            {formatNumber(data?.price - (data?.price * data.discount) / 100)}
-                        </p>
-                    </div>
-                ) : (
-                    <div className="box-card-product--content--monney">
-                        <p className="box-card-product--content--monney--sale">
-                            {formatNumber(data?.price)}
-                        </p>
-                    </div>
-                )}
-
+                    )}
+                </div>
                 <div className="box-card-product--content--loader">
                     <div className="box-card-product--content--loader--color">
                         Còn {data?.quantity}
